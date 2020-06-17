cd4c0124b20a8b0181ed1cd26b6919de23a7f581
Lucas.B <62809823+lucasb1993955@users.noreply.github.com>
2020-06-17 00:27:07 -0300

    Add files via upload

--- funciones.h
+++ funciones.h
@@ -21,31 +21,29 @@ void crear_pantalla(){
 
 void unir_pantalla(){
     SDL_FillRect(screen,0,SDL_MapRGB(screen->format, 1000,0,1000));
-  SDL_BlitSurface( inicio, NULL, screen, NULL);
-        SDL_Flip( screen );
+    SDL_BlitSurface( inicio, NULL, screen, NULL);
+        SDL_Flip(screen);
 
 }
 
 void jugar(){
-    int i=0;
     SDL_Rect Pos_Imag;
     Pos_Imag.x=0;
     Pos_Imag.y=0;
-    Pos_Imag.h=180;
-    Pos_Imag.w=180;
+    Pos_Imag.h=600;///alto
+    Pos_Imag.w=800;///ancho
         while(true)
         {
         SDL_FillRect(screen,0,SDL_MapRGB(screen->format, 1000,0,1000));
-        SDL_BlitSurface(InicioDeJuego, NULL, screen, NULL);
-        while(i!=100){
-                i++;
+        while(Pos_Imag.y!=-600){
+
         Pos_Imag.y=Pos_Imag.y-10;
         SDL_BlitSurface(InicioDeJuego,NULL,screen,NULL);
         SDL_BlitSurface(Bloque,&Pos_Imag,screen,NULL);
         SDL_Flip(screen);
-        SDL_Flip(Bloque);
         SDL_Delay(200);
         }
+        return;
         }
 }
 
